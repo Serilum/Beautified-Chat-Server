ea27ccf896e3c75e94279cf83545ed9bf35471d3
Rick South <rick@natamus.com>
2023-12-12 10:59:33 +0100

    Beautified Chat Server source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/beautifiedchatserver/events/BeautifulChatEvent.java
@@ -0,0 +1,53 @@
+package com.natamus.beautifiedchatserver.events;
+
+import com.mojang.datafixers.util.Pair;
+import com.natamus.beautifiedchatserver.config.ConfigHandler;
+import com.natamus.beautifiedchatserver.util.Util;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class BeautifulChatEvent {
+	public static Pair<Boolean, Component> onServerChat(ServerPlayer serverPlayer, Component messageComponent, UUID uuid) {
+		String timestamp = new SimpleDateFormat(ConfigHandler.timestampFormat).format(new Date());
+		
+		String user = serverPlayer.getName().getString();
+		String message = messageComponent.getString();
+		if (!message.contains(user)) {
+			return null;
+		}
+		
+		if (message.contains("> ")) {
+			message = message.substring(message.split("> ")[0].length() + 2);
+		}
+		
+		MutableComponent output = new TextComponent("");
+		String raw_outputstring = ConfigHandler.chatMessageFormat;
+		for (String word : raw_outputstring.split("%")) {
+			ChatFormatting colour = Util.getColour(word);
+			String toappend = word;
+			
+			if (word.equalsIgnoreCase("timestamp")) {
+				toappend = timestamp;
+			}
+			else if (word.equalsIgnoreCase("username")) {
+				toappend = user;
+			}
+			else if (word.equalsIgnoreCase("chatmessage")) {
+				toappend = message;
+			}
+			
+			MutableComponent wordcomponent = new TextComponent(toappend);
+			wordcomponent.withStyle(colour);
+			output.append(wordcomponent);
+		}
+
+		return new Pair<Boolean, Component>(true, output);
+	}
+}
