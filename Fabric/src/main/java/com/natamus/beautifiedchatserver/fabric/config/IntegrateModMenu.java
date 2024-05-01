1ef625caea0b1b07255d9771a4a490f0df6d8ea0
Rick South <rick@natamus.com>
2024-05-01 16:46:12 +0200

    Beautified Chat Server source code for 1.20.6.

---
+++ Fabric/src/main/java/com/natamus/beautifiedchatserver/fabric/config/IntegrateModMenu.java
@@ -0,0 +1,13 @@
+package com.natamus.beautifiedchatserver.fabric.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.beautifiedchatserver.util.Reference;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class IntegrateModMenu implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> DuskConfig.DuskConfigScreen.getScreen(parent, Reference.MOD_ID);
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/beautifiedchatserver/neoforge/events/NeoForgeBeautifulChatEvent.java
@@ -0,0 +1,37 @@
+package com.natamus.beautifiedchatserver.neoforge.events;
+
+import com.mojang.datafixers.util.Pair;
+import com.natamus.beautifiedchatserver.data.Constants;
+import com.natamus.beautifiedchatserver.events.BeautifulChatEvent;
+import com.natamus.collective.functions.MessageFunctions;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.neoforge.event.ServerChatEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeBeautifulChatEvent {
+	@SubscribeEvent
+	public static void onServerChat(ServerChatEvent e) {
+		ServerPlayer serverPlayer = e.getPlayer();
+		Component originalMessage = e.getMessage();
+		MutableComponent fullMessage = Component.literal("<" + serverPlayer.getName().getString() + "> ").append(originalMessage);
+
+		Pair<Boolean, Component> pair = BeautifulChatEvent.onServerChat(serverPlayer, fullMessage, serverPlayer.getUUID());
+		if (pair != null) {
+			if (pair.getFirst()) {
+				MutableComponent newMessage = pair.getSecond().copy();
+				if (fullMessage != newMessage) {
+					e.setCanceled(true);
+
+					serverPlayer.server.execute(() -> {
+						Constants.logger.info(newMessage.getString());
+						MessageFunctions.broadcastMessage(serverPlayer.level(), newMessage);
+					});
+				}
+			}
+		}
+	}
+}
