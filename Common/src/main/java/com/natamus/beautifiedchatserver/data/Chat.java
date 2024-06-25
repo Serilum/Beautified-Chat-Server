0293422018823e464bb6bb12f0ca5f84594aa7eb
Rick South <rick@natamus.com>
2024-06-25 22:03:42 +0200

    (v2.5) Fixed chat messages not being logged in the server.

--- Common/src/main/java/com/natamus/beautifiedchatserver/data/Chat.java
+++ Common/src/main/java/com/natamus/beautifiedchatserver/data/Chat.java
@@ -3,6 +3,6 @@ package com.natamus.beautifiedchatserver.data;
 import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
 
-public class Constants {
+public class Chat {
     public static final Logger logger = LogUtils.getLogger();
 }
