db6d4bc7fe4550e85ccc1b0a9644a38eda4d1a15
Rick South <rick@natamus.com>
2025-06-19 04:05:54 +0200

    Beautified Chat Server source code for 1.21.6.

---
+++ Forge/src/main/java/com/natamus/beautifiedchatserver/forge/events/ForgeBeautifulChatEvent.java
@@ -0,0 +1,42 @@
+package com.natamus.beautifiedchatserver.forge.events;
+
+import com.mojang.datafixers.util.Pair;
+import com.natamus.beautifiedchatserver.data.Chat;
+import com.natamus.beautifiedchatserver.events.BeautifulChatEvent;
+import com.natamus.collective.functions.MessageFunctions;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+public class ForgeBeautifulChatEvent {
+	public static void registerEventsInBus() {
+		// BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeBeautifulChatEvent.class);
+
+		ServerChatEvent.BUS.addListener(ForgeBeautifulChatEvent::onServerChat);
+	}
+
+	@SubscribeEvent
+	public static boolean onServerChat(ServerChatEvent e) {
+		ServerPlayer serverPlayer = e.getPlayer();
+		Component originalMessage = e.getMessage();
+		MutableComponent fullMessage = Component.literal("<" + serverPlayer.getName().getString() + "> ").append(originalMessage);
+
+		Pair<Boolean, Component> pair = BeautifulChatEvent.onServerChat(serverPlayer, fullMessage, serverPlayer.getUUID());
+		if (pair != null) {
+			if (pair.getFirst()) {
+				MutableComponent newMessage = pair.getSecond().copy();
+				if (fullMessage != newMessage) {
+					serverPlayer.getServer().execute(() -> {
+						Chat.logger.info(newMessage.getString());
+						MessageFunctions.broadcastMessage(serverPlayer.level(), newMessage);
+					});
+
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
