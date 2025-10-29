367bfee9847334caabbb248ba40e3e2e4efb16b0
Rick South <rick@natamus.com>
2025-10-29 14:23:36 +0100

    (v3.1) (Forge/Neo) Fixed messages being shown twice in the server console.

--- NeoForge/src/main/java/com/natamus/beautifiedchatserver/neoforge/events/NeoForgeBeautifulChatEvent.java
+++ NeoForge/src/main/java/com/natamus/beautifiedchatserver/neoforge/events/NeoForgeBeautifulChatEvent.java
@@ -1,14 +1,13 @@
 package com.natamus.beautifiedchatserver.neoforge.events;
 
 import com.mojang.datafixers.util.Pair;
-import com.natamus.beautifiedchatserver.data.Chat;
 import com.natamus.beautifiedchatserver.events.BeautifulChatEvent;
 import com.natamus.collective.functions.MessageFunctions;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.level.ServerPlayer;
-import net.neoforged.neoforge.event.ServerChatEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.ServerChatEvent;
 
 public class NeoForgeBeautifulChatEvent {
 	@SubscribeEvent
@@ -25,7 +24,6 @@ public class NeoForgeBeautifulChatEvent {
 					e.setCanceled(true);
 
 					serverPlayer.server.execute(() -> {
-						Chat.logger.info(newMessage.getString());
 						MessageFunctions.broadcastMessage(serverPlayer.level(), newMessage);
 					});
 				}
